95495275be929517a5e91eb8996a1dd48ccb9cf5
hyh <buzhiming_hao@163.com>
2022-07-28 15:50:35 +0800

    add import;

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
